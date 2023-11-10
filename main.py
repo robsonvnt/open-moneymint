0cd2c52f9cbffbb9fb1faa2f779dcb5bbd7167ae
Robson Ventura Rodrigues <cadastro_rvr@pm.me>
2023-11-10 16:28:13 -0300

    Monitoramento com Sentry

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
+import sentry_sdk
 
 from investment.interface.portfolio_http import router as portfolio_router
 from investment.interface.investment_http import router as investment_router
 from investment.interface.consolidated_balance_http import router as consolidated_balance_router
+
+sentry_sdk.init(
+    dsn="https://da079e56c790d3f9dbe745763e3f73e6@o4506203208744960.ingest.sentry.io/4506203210973184",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 
 app = FastAPI()
 
