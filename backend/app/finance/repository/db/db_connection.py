5f584fadfdfc4fc3f065c81751d6cf6df0d1eff1
Robson Ventura Rodrigues <cadastro_rvr@pm.me>
2023-11-27 19:53:01 -0300

    Ajuste para não estourar o número de conexões

--- backend/app/finance/repository/db/db_connection.py
+++ backend/app/finance/repository/db/db_connection.py
@@ -3,11 +3,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+db_url = os.getenv("DATABASE_URL")
+engine = create_engine(db_url, pool_size=20, pool_recycle=3600)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db_session():
-    db_url = os.getenv("DATABASE_URL")
-    engine = create_engine(db_url)
-    session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = session()
     try:
         yield db
