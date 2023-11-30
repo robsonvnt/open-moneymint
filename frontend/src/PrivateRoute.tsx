47d85d0dbca9b9d7742b79cc1be845ecc99c12d5
Robson Ventura Rodrigues <cadastro_rvr@pm.me>
2023-11-30 18:34:44 -0300

    Fix na tela de login

--- frontend/src/PrivateRoute.tsx
+++ frontend/src/PrivateRoute.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from './auth/AuthContext';
 
 interface PrivateRouteProps {
-  children: JSX.Element;
+  children: React.JSX.Element;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { accessToken } = useAuth();
   const location = useLocation();
+
+  let accessToken = localStorage.getItem('accessToken');
 
   if (!accessToken) {
     return <Navigate to="/login" state={{ from: location }} replace />;
